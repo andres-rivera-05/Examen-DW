d9a221a98e2155853912cc3359380a6f676aa4cf
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-14 21:07:45 -0600

    ..

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
+//const express = require('express')
+import express from 'express';
+import { libro } from './routes/apiLibros.js'
 const app = express();
+//middlewares
+app.use(express.json());
+
 const port = 4000;
+
+app.use('/api/libro', libro)
 
 app.listen(port, ()=>{
 
     console.log(`Escuchando en el puerto ${port}`)
-    
+
 })
