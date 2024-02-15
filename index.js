e52ba9423d51b8292c1e538b742c52c023742784
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-15 11:48:50 -0600

    index

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import { libro } from './routes/apiLibros.js'
 import { usuario } from './routes/apiUsuarios.js'
 import { reserva } from './routes/apiReservas.js';
+import { categoria } from './routes/apiCategorias.js';
+import { libroCategoria  } from './routes/apiLibroCategoria.js';
 const app = express();
 //middlewares esto hace que se reconosca el formato json
 app.use(express.json());
@@ -12,6 +14,9 @@ const port = 4000;
 app.use('/api/libro', libro)
 app.use('/api/usuario', usuario)
 app.use('/api/reserva', reserva )
+app.use('/api/categoria', categoria)
+app.use('/api/categorias', libroCategoria)
+
 
 app.listen(port, ()=>{
     console.log(`Escuchando en el puerto ${port}`)
