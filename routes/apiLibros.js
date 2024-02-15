fe024d104905d18db0581b2bd480c2e16e94d58a
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-14 20:46:31 -0600

    metodos de api completados

--- routes/apiLibros.js
+++ routes/apiLibros.js
@@ -1,15 +1,46 @@
 import express from 'express'
 const libro = express();
+import { db } from '../db/conn.js'
 
 
-libro.get('', (req, res)=>{
+libro.get('', async (req, res)=>{
 
+    const sql = `Select * from tbl_libro order by id`;
+    const result = await db.query(sql)
+    res.json(result);
+    
 })
 
-libro.post('',(req,res)=>{
+libro.post('', async (req,res)=>{
  
+    const { titulo, autor, anio_publicacion  } = req.body;
+    const params = [titulo, autor, anio_publicacion];
 
+    const sql =`insert into tbl_libro
+                (titulo, autor, anio_publicacion)
+                values
+                ($1, $2, $3) returning * `;
 
+    const result = await db.query(sql, params);
+    res.json(result);
+})
+
+libro.put('/:id', async  (req, res)=>{
+
+    const { titulo, autor, anio_publicacion  } = req.body;
+    const { id } = req.params;
+
+    const params = [titulo, autor, anio_publicacion, id];
+
+    const sql = `update tbl_libro set
+                    titulo = $1,
+                    autor = $2,
+                    anio_publicacion = $3
+                where id = $4 returning *`;
+
+    const result = await db.query(sql, params);
+
+    res.json(result);
 })
 
 export { libro }
