883c9cd1ac40bbbabb9c6047102ab8aa936bfc18
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-15 11:49:15 -0600

    modificando el sql

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -2,7 +2,11 @@ import { db } from '../db/conn.js'
 
 const getReserva = async (req, res)=>{
 
-    const sql = `Select * from tbl_reserva order by id`;
+    //const sql = `Select * from tbl_reserva order by id`;
+    const sql = `SELECT r.id, u.id as usuario_id, u.nombre as nombre_usuario, l.titulo as titulo_libro, r.fecha_reserva, r.fecha_devolucion, r.estado
+    FROM tbl_reserva r
+    INNER JOIN tbl_usuario u ON r.usuario_id = u.id
+    INNER JOIN tbl_libro l ON r.libro_id = l.id`
     const result = await db.query(sql)
     res.json(result);
 
@@ -10,13 +14,13 @@ const getReserva = async (req, res)=>{
 
 const postReserva = async (req, res)=>{
 
-    const { usuario_id, libro_id, fecha_reserva, fecha_devolucion  } = req.body;
-    const params = [usuario_id, libro_id, fecha_reserva, fecha_devolucion];
+    const { usuario_id, libro_id, fecha_reserva, fecha_devolucion, estado  } = req.body;
+    const params = [usuario_id, libro_id, fecha_reserva, fecha_devolucion, estado];
 
     const sql =`insert into tbl_reserva
-                (usuario_id, libro_id, fecha_reserva, fecha_devolucion)
+                (usuario_id, libro_id, fecha_reserva, fecha_devolucion, estado)
                 values
-                ($1, $2, $3, $4) returning * `;
+                ($1, $2, $3, $4, $5) returning * `;
 
     const result = await db.query(sql, params);
 
@@ -25,16 +29,17 @@ const postReserva = async (req, res)=>{
 }
 
 const putReserva = async (req, res)=>{
-    const { usuario_id, libro_id, fecha_reserva, fecha_devolucion } = req.body;
+    const { usuario_id, libro_id, fecha_reserva, fecha_devolucion, estado } = req.body;
     const { id } = req.params;
-    const params = [usuario_id, libro_id, fecha_reserva, fecha_devolucion, id];
+    const params = [usuario_id, libro_id, fecha_reserva, fecha_devolucion, estado, id];
 
     const sql = `update tbl_reserva set
                     usuario_id = $1,
                     libro_id = $2,
                     fecha_reserva = $3,
-                    fecha_devolucion = $4
-                 where id = $5 returning *`;
+                    fecha_devolucion = $4,
+                    estado = $5
+                 where id = $6 returning *`;
 
     const result = await db.query(sql, params);
 
