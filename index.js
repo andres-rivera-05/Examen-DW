52f5cf9d06e73b97cdd51ed2254f921aa2f1f4f5
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-14 23:50:10 -0600

    agregando app reserva

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 //const express = require('express')
 import express from 'express';
 import { libro } from './routes/apiLibros.js'
+import { usuario } from './routes/apiUsuarios.js'
+import { reserva } from './routes/apiReservas.js';
 const app = express();
-//middlewares
+//middlewares esto hace que se reconosca el formato json
 app.use(express.json());
-
+//creacmos el puerto
 const port = 4000;
 
 app.use('/api/libro', libro)
+app.use('/api/usuario', usuario)
+app.use('/api/reserva', reserva )
 
 app.listen(port, ()=>{
-
     console.log(`Escuchando en el puerto ${port}`)
-
 })
