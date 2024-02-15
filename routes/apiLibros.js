b54deac98db175556472d3579eb73564073b505b
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-14 21:08:00 -0600

    modificando

--- routes/apiLibros.js
+++ routes/apiLibros.js
@@ -1,46 +1,14 @@
 import express from 'express'
 const libro = express();
-import { db } from '../db/conn.js'
+import { getLibro, postLibro, putLibro, deleteLibro } from '../controllers/librosController.js';
 
 
-libro.get('', async (req, res)=>{
+libro.get('', getLibro )
 
-    const sql = `Select * from tbl_libro order by id`;
-    const result = await db.query(sql)
-    res.json(result);
-    
-})
+libro.post('', postLibro )
 
-libro.post('', async (req,res)=>{
- 
-    const { titulo, autor, anio_publicacion  } = req.body;
-    const params = [titulo, autor, anio_publicacion];
+libro.put('/:id', putLibro )
 
-    const sql =`insert into tbl_libro
-                (titulo, autor, anio_publicacion)
-                values
-                ($1, $2, $3) returning * `;
-
-    const result = await db.query(sql, params);
-    res.json(result);
-})
-
-libro.put('/:id', async  (req, res)=>{
-
-    const { titulo, autor, anio_publicacion  } = req.body;
-    const { id } = req.params;
-
-    const params = [titulo, autor, anio_publicacion, id];
-
-    const sql = `update tbl_libro set
-                    titulo = $1,
-                    autor = $2,
-                    anio_publicacion = $3
-                where id = $4 returning *`;
-
-    const result = await db.query(sql, params);
-
-    res.json(result);
-})
+libro.delete('/:id', deleteLibro )
 
 export { libro }
